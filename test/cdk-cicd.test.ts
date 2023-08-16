a338cbff1956bd691e6c8134f1f62c2410dcfcfb
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-08-16 16:49:50 +0100

    Add unit tests

--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -1,2 +1,9 @@
-test('initial test', () => {
+import {handler} from "../services/hello";
+
+describe('Hello describe test suite', () => {
+    it('should return 200', async () => {
+        const result = await handler({}, {});
+        
+        expect(result.statusCode).toBe(200);
+    })
 });
